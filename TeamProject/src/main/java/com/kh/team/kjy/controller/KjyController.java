f57a8fe2c1c6c9370c95cdeafb2088ddec1577f2
KH <KH@KH-PC>
2020-07-06 14:50:18 +0900

    kjs

---
+++ TeamProject/src/main/java/com/kh/team/kjy/controller/KjyController.java
@@ -0,0 +1,14 @@
+package com.kh.team.kjy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/kjy")
+public class KjyController {
+
+	@RequestMapping("/test")
+	public void test() throws Exception {
+		
+	}
+}

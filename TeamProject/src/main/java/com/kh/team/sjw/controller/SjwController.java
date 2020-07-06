13312f46ac8b3eb14a51b437c6db2098eaf8d3ed
KH <KH@KH-PC>
2020-07-06 14:59:36 +0900

    sjw commit

---
+++ TeamProject/src/main/java/com/kh/team/sjw/controller/SjwController.java
@@ -0,0 +1,14 @@
+package com.kh.team.sjw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sjw")
+public class SjwController {
+
+	@RequestMapping("/test")
+	public void test() throws Exception {
+		
+	}
+}
